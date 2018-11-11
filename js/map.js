37e03a4b10689b96115576d34ae476172604ca3a
JakerHuber <30348496+JakerHuber@users.noreply.github.com>
2018-11-10 21:51:08 -0600

    Adding fuction to js for placing and loading markers

--- js/map.js
+++ js/map.js
@@ -1,7 +1,34 @@
           var map;
           function initMap() {
-            map = new google.maps.Map(document.getElementById('canvas3'), {
+			  //initializes map
+			  map = new google.maps.Map(document.getElementById('canvas3'), {
               center: {lat: -34.397, lng: 150.644},
-              zoom: 8
+              zoom: 2,
+			  mapTypeId: 'terrain'
             });
+			
+					
+			// Create a <script> tag and set the USGS URL as the source.
+			var script = document.createElement('script');
+			script.src = 'js/jacob_GeoJSONP.js';
+			document.getElementsByTagName('head')[0].appendChild(script);
+			
+			// create markers if i feel like it
+			
+			
           }
+		  
+		  // Loop through the results array and place a marker for each
+			// set of coordinates.
+			window.eqfeed_callback = function(results) {
+				for (var i = 0; i < results.features.length; i++) {
+				var coords = results.features[i].geometry.coordinates;
+				var latLng = new google.maps.LatLng(coords[1],coords[0]);
+				var marker = new google.maps.Marker({
+					position: latLng,
+					map: map
+					});
+				}
+			}
+			
+			
